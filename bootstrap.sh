060be8715e16e3437fc41eb2a8456d8a28dd0b1f
Frank Kleine <mikey@bovigo.org>
2022-12-06 20:17:59 +0100

    Fix clone URL

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@ set -eu
 
 ### Install homeshick ###
 if [ ! -d $HOME/.homesick/repos/homeshick ]; then
-  git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
+  git clone https://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
 fi
 source $HOME/.homesick/repos/homeshick/homeshick.sh
 
